896e2cafcb64d76d78aa07d80cd573a421af349e
Brendan O'Brien <bobrien@pursuit.org>
2024-12-31 17:10:43 -0500

    adds more comments

--- hello.go
+++ hello.go
@@ -3,5 +3,5 @@ package main // always import the main package
 import "fmt" // import the fmt package
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!") // prints hello world
 }
